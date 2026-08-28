4c22da38dfb7059939cfd28614d0e9549e624456
Halimatu Sadia Mohammed <h.mohammed@alustudent.com>
2026-08-28 21:46:56 +0400

    Created hero table

--- database.sql
+++ database.sql
@@ -18,3 +18,20 @@ Create table users (
 insert into users (username, password) values 
 ('professorx', '2b$10$Kn9JZAy3tW0QTum9Y7zHPuuh5hFV.PZfBofYF4BuCrto99vewA5dy');
 
+--- Table: heroes ---
+--- This table stores the records of the X-Men heroes managed by Professor Xavier.
+Drop table if exists heroes;
+Create table heroes (
+    hero_id int not null auto_increment primary key,
+    hero_name varchar(100) not null,
+    real_name varchar(100) not null,
+    short_bio VARCHAR(255) NOT NULL,
+    long_bio TEXT NOT NULL,
+    powers VARCHAR(255),
+    team VARCHAR(100),
+    publisher VARCHAR(100) DEFAULT 'Marvel Comics',
+    gender VARCHAR(20),
+    status VARCHAR(20) DEFAULT 'Active',
+    image_url VARCHAR(255),
+    date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
