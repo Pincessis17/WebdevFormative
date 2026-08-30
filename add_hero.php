725f450dff8b19da489eb7217323d501bb18e504
tks <t.sauntoo@alustudent.com>
2026-08-30 11:41:34 +0400

    Added the php-sql code to insert the data into the database. Fixed small typo bug

--- add_hero.php
+++ add_hero.php
@@ -12,7 +12,7 @@ $values = [
     'long_bio' => '',
     'powers' => '',
     'team' => '',
-    'publishers' => 'Marvel Comics',
+    'publisher' => 'Marvel Comics',
     'gender' => '',
     'status' => 'Active',
     'image_url' => '',
@@ -26,7 +26,24 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $values[$key] = trim($_POST[$key] ?? $default);
     }
 
-    
+    //if any is empty, assign error message to $error
+    if ($values['hero_name'] === '' ||  $values['real_name'] === '' ||$values['short_bio'] === '' ||$values['long_bio'] === '' ) {
+        $error = 'Hero Name, Real Name, Short Biography and Long Biography are all required. Please fill all of them.'
+    } else {
+        //prepare data for processing
+        $stmt = $pdo -> prepare("
+            INSERT INTO heroes(hero_name, real_name, short_bio, long_bio, powers, team, publisher, gender, status, image_url)
+            VALUES (:hero_name, :real_name, :short_bio, :long_bio, :powers, :team, :publisher, :gender, :status, :image_url)
+        ");
+
+        //Processes the prepared data
+        $stmt -> execute($values);
+
+        //redirects user to index.php, the main page
+        header('Location: index.php');
+        exit; //exits cleanly
+
+    }
 
 }
 
