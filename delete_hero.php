a768dc6e68d03274e360c92c222a473670ace926
tks <t.sauntoo@alustudent.com>
2026-08-30 17:47:16 +0400

    Added unsaved  delete_hero progress

--- delete_hero.php
+++ delete_hero.php
@@ -5,7 +5,14 @@ require_once 'includes/auth.php';
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $id = (int) ($_POST['id'] ?? 0);
 
-    
-
+    if ($id > 0 ) {
+        //Prepares data for deletion
+        $stmt = $pdo -> prepare("DELETE FROM heroes WHERE id = :id");
+        //executes the deletion
+        $stmt -> execute(['id' => $id]);
+    }
 
 }
+
+header('Location: index.php?deleted=1');
+exit;
